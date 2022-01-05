2decf341a2f39c8cab0ffda7d164439ef5e3c182
Marc Apesos <apesos2013@gmail.com>
2022-01-05 14:50:16 -0500

    got rid of promise.all. gnewsAPI throw 429 too easily

--- src/actions/news.js
+++ src/actions/news.js
@@ -3,21 +3,19 @@ import newsAPI from "../apis/newsAPI";
 export const getNews = (stocks, listId) => async (dispatch) => {
   let newsArr = [];
 
-  await Promise.all(
-    stocks.map(async (ticker) => {
-      return await newsAPI
-        .get("", {
-          params: {
-            q: ticker,
-            lang: "en",
-          },
-        })
-        .then((res) => {
-          newsArr.push(...res.data.articles.slice(0, 3));
-          return res.data.articles.slice(0, 3);
-        });
-    })
-  );
+  stocks.map(async (ticker) => {
+    return await newsAPI
+      .get("", {
+        params: {
+          q: ticker,
+          lang: "en",
+        },
+      })
+      .then((res) => {
+        newsArr.push(...res.data.articles.slice(0, 3));
+        return res.data.articles.slice(0, 3);
+      });
+  });
 
   // assigns each array of articles to key of listId. This can be accessed easily within ShowNews component
   dispatch({
