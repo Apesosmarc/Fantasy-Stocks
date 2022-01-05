fc033c633c348c8128fc97981124cc7b9676d5f0
Marc Apesos <apesos2013@gmail.com>
2022-01-05 14:55:12 -0500

    promise.all catch

--- src/actions/news.js
+++ src/actions/news.js
@@ -3,19 +3,23 @@ import newsAPI from "../apis/newsAPI";
 export const getNews = (stocks, listId) => async (dispatch) => {
   let newsArr = [];
 
-  stocks.map(async (ticker) => {
-    return await newsAPI
-      .get("", {
-        params: {
-          q: ticker,
-          lang: "en",
-        },
+  Promise.all(
+    stocks
+      .map(async (ticker) => {
+        return await newsAPI
+          .get("", {
+            params: {
+              q: ticker,
+              lang: "en",
+            },
+          })
+          .then((res) => {
+            newsArr.push(...res.data.articles.slice(0, 3));
+            return res.data.articles.slice(0, 3);
+          });
       })
-      .then((res) => {
-        newsArr.push(...res.data.articles.slice(0, 3));
-        return res.data.articles.slice(0, 3);
-      });
-  });
+      .catch((e) => console.log(e))
+  );
 
   // assigns each array of articles to key of listId. This can be accessed easily within ShowNews component
   dispatch({
